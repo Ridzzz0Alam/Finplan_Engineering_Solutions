c321c6ba7ffdfbb07644213163b7fa3e8675a21f
Mohammad Reduwan <ridwan0gts@gmail.com>
2025-03-20 01:13:27 +0600

    added team.js as about us

---
+++ sections/Team.js
@@ -0,0 +1,28 @@
+import { Title } from "@/components/common/Title";
+import React from "react";
+
+const Team = () => {
+  return (
+    <>
+      <section className="agency bg-top">
+        <div className="container">
+          <div className="heading-title">
+            <Title title="ABOUT US" />
+            <p>
+              We offer engineering solutions for design, manufacturing, and
+              inspection processes, ensuring compliance with international
+              standards and providing project-specific support to meet customer
+              requirements.
+            </p>
+          </div>
+        </div>
+        <br />
+        <br />
+        <br />
+        <br />
+      </section>
+    </>
+  );
+};
+
+export default Team;
